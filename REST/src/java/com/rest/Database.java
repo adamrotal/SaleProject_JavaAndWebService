e099fed7a97bcf8d393a4696b2f765170c8637aa
Ahmad Fajar Prasetiyo <ahmad.fajar260695@gmail.com>
2016-11-11 22:25:58 +0700

    insert token to database

---
+++ REST/src/java/com/rest/Database.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rest;
+import java.sql.* ;  
+
+/**
+ *
+ * @author afp
+ */
+public class Database {
+    private final String URL = "jdbc:mysql://localhost:3306/onlineshop?zeroDateTimeBehavior=convertToNull";
+    private final String USER = "kuliah";
+    private final String PASS = "kuliah";
+    private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static Database addr = null;
+    
+    private Database() {
+    
+    }
+    
+    public static Database instance() {
+        if(addr == null) {
+            addr = new Database();
+        }
+        
+        return addr;
+    }
+    
+    public void insertToken(String token, long waktu, String key) throws ClassNotFoundException, SQLException {
+        
+        // Creating Connection
+        Class.forName(JDBC_DRIVER);
+        Connection connection = DriverManager.getConnection(URL, USER, PASS);
+        
+        // Creating statement
+        Statement statement = connection.createStatement();
+        String sql;
+        sql = "UPDATE user SET token="+token+", tanggalExp="+waktu+" WHERE email="+key+" OR username="+key;
+        ResultSet resultSet = statement.executeQuery(sql);
+    }
+}
--- REST/src/java/com/rest/Token.java
+++ REST/src/java/com/rest/Token.java
@@ -5,6 +5,8 @@
  */
 package com.rest;
 
+import java.sql.SQLException;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -13,7 +15,8 @@ import java.util.Random;
  */
 public class Token {
     static private Token addr = null;
-    static private final int PANJANG = 15;
+    static private Database database = null;
+    private final int PANJANG = 15;
     
     private Token() {
         
@@ -22,6 +25,7 @@ public class Token {
     static public Token instance() {
         if(addr == null) {
             addr = new Token();
+            database = Database.instance();
         }
         
         return addr;
@@ -29,10 +33,10 @@ public class Token {
     
     public boolean isValidToken(String token) {
         
-        return false;
+        return true;
     }
     
-    public String generateToken(String email) {
+    public String generateToken(String key) throws ClassNotFoundException, SQLException {
         String seed = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxz1234567890";
         String token = "";
         Random rand = new Random();
@@ -44,11 +48,18 @@ public class Token {
                 ind = rand.nextInt(seed.length());
                 token = token + seed.charAt(ind);
             }
-        } while (isValidToken(token));
+        } while (false);// isValidToken(token));
         
-        
-        
+        Date date = new Date();
+        long ms;
+        ms = date.getTime();
+        ms = ms + menitKeMs(10);
+        database.insertToken(token, ms, key);
         return token;
+    }
+    
+    private int menitKeMs(int menit) {
+        return menit * 60 * 1000;
     }
     
 }
