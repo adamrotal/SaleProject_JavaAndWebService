97af2d4e530a2090f89cd565ba76d6bff52bcb7a
Dandu Satyanuraga <13515601@std.stei.itb.ac.id>
2016-11-11 10:55:50 +0700

    add Database.java

---
+++ servlet/Database.java
@@ -0,0 +1,9 @@
+package bean;
+
+
+public interface Database {
+    String DRIVER="com.mysql.jdbc.Driver";
+    String CONNECTION_URL="jdbc:mysql://localhost:3306/wbd";
+    String USERNAME="root";
+    String PASSWORD="";
+}
